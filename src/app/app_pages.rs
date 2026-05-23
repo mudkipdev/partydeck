1b6cc985bd6808c8c1f8b48dc4d5c04254afadad
cseelhoff <cseelhoff@gmail.com>
2026-05-23 10:55:28 -0500

    fix: update checkbox label for PS controllers hidraw option

--- src/app/app_pages.rs
+++ src/app/app_pages.rs
@@ -258,7 +258,7 @@ impl PartyApp {
         }
 
         if h.win() {
-            ui.checkbox(&mut h.enable_hidraw_ps, "Enable hidraw for PS controllers (needed for Unity new Input System games e.g. V Rising; may cause double input in some games)");
+            ui.checkbox(&mut h.enable_hidraw_ps, "Enable hidraw for PS controllers (needed for Unity new Input System games; may cause double input in some games)");
         }
 
         if !h.win() {
